52d31cdf8d2c24aedc20bf7e8d6fb06a52161bd5
Andrew Thompson <andrew@hijacked.us>
2021-12-22 11:59:30 -0800

    Remove inactive hotspots from h3dex, enable it as a targeting source

--- src/blockchain.erl
+++ src/blockchain.erl
@@ -3028,6 +3028,8 @@ run_gc_hooks(Blockchain, _Hash) ->
 
         ok = blockchain_ledger_v1:maybe_gc_scs(Blockchain, Ledger),
 
+        ok = blockchain_ledger_v1:maybe_gc_h3dex(Ledger),
+
         ok = blockchain_ledger_v1:maybe_recalc_price(Blockchain, Ledger) %,
 
         %% ok = blockchain_ledger_v1:refresh_gateway_witnesses(Hash, Ledger)
